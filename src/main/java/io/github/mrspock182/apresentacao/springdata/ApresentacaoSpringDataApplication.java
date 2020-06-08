0413d6f8eda630409a249c81e0b7219ac00fbac6
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-06-08 19:11:22 -0300

    project start

--- src/main/java/io/github/mrspock182/apresentacao/springdata/ApresentacaoSpringDataApplication.java
+++ src/main/java/io/github/mrspock182/apresentacao/springdata/ApresentacaoSpringDataApplication.java
@@ -1,73 +1,13 @@
 package io.github.mrspock182.apresentacao.springdata;
 
-import io.github.mrspock182.apresentacao.springdata.service.FuncaoCargo;
-import io.github.mrspock182.apresentacao.springdata.service.FuncaoFuncionario;
-import io.github.mrspock182.apresentacao.springdata.service.FuncaoRelatorios;
-import io.github.mrspock182.apresentacao.springdata.service.FuncaoUnidade;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
-@EnableJpaRepositories
 @SpringBootApplication
-public class ApresentacaoSpringDataApplication implements CommandLineRunner {
-
-	private Boolean system = true;
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-	@Autowired
-	private FuncaoCargo funcaoCargo;
-
-	@Autowired
-	private FuncaoUnidade funcaoUnidade;
-
-	@Autowired
-	private FuncaoRelatorios funcaoRelatorios;
-
-	@Autowired
-	private FuncaoFuncionario funcaoFuncionario;
+public class ApresentacaoSpringDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApresentacaoSpringDataApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-
-		while (system) {
-			System.out.println("Qual função deseja executar?");
-			System.out.println("1 - Funcionario");
-			System.out.println("2 - Cargo");
-			System.out.println("3 - Unidade");
-			System.out.println("4 - Relatorios");
-			System.out.println("5 - Sair");
-
-			Integer function = scanner.nextInt();
-
-			switch (function) {
-				case 1:
-					funcaoFuncionario.inicio(scanner);
-					break;
-				case 2:
-					funcaoCargo.inicio(scanner);
-					break;
-				case 3:
-					funcaoUnidade.inicio(scanner);
-					break;
-				case 4:
-					funcaoRelatorios.inicio(scanner);
-					break;
-				default:
-					System.out.println("Finalizando");
-					system = false;
-					break;
-			}
-		}
-
-	}
 }
--- src/main/java/io/github/mrspock182/apresentacao/springdata/repository/CargoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.mrspock182.apresentacao.springdata.repository;
-
-import io.github.mrspock182.apresentacao.springdata.domain.Cargo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CargoRepository extends JpaRepository<Cargo, Integer> {
-}
